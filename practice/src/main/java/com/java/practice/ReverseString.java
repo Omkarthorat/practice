d88fc348e10889b833f04094f6b4c623b1d43668
HP <HP@DESKTOP-5H7JUEO>
2018-07-31 11:39:48 +0530

    Reverse String

---
+++ practice/src/main/java/com/java/practice/ReverseString.java
@@ -0,0 +1,14 @@
+package com.java.practice;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		
+		StringBuilder sb= new StringBuilder("omkar");
+		
+		
+		System.out.println(sb.reverse());		
+		
+	}
+
+}
